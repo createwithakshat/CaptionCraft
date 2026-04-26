204277d0f829408502f403bbb7fd4d53ec5477d1
Akshat Aswal <96677252+createwithakshat@users.noreply.github.com>
2026-04-26 13:50:47 +0530

    Update backend api to enable CORS

--- backend/app.py
+++ backend/app.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # Enable CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["https://captioncraft-cal6.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
